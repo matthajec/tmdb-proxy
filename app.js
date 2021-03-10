25e4e239029fa7213e66bece106d806c4f1f2297
matt hajec <matthew.hajec02@gmail.com>
2021-03-09 20:18:44 -0500

    cors

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 require('dotenv').config();
 const app = require('express')();
 const fetch = require('node-fetch');
+const rateLimit = require("express-rate-limit");
+const cors = require('cors');
 
+// limit requests to 20 per minute
+app.use(rateLimit({
+  windowMs: '60000',
+  max: 20
+}));
+
+app.use(cors());
+
+// get this weeks trending movies
 app.get('/trending', (req, res) => {
   fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=${process.env.API_KEY}`)
     .then(res => res.json())
     .then(data => res.json(data));
 });
 
+
+// perform a query
 app.get('/search', (req, res) => {
   fetch(`https://api.themoviedb.org/3/search/movie?query=${req.query.query}&page=${req.query.page}&include_adult=false&api_key=${process.env.API_KEY}`)
     .then(res => res.json())
